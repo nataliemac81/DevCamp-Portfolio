04cff413ca7b78501c9235bf8a4aba0f20a41442
Natalie McCroy <nataliemccroy@Natalies-MacBook-Pro.local>
2022-06-10 12:11:27 -0500

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}" ,
+    body: "Donut candy canes ice cream bear claw carrot cake sweet. Apple pie bear claw marzipan cotton candy bonbon donut macaroon cake. Soufflé candy canes cupcake chocolate gummies brownie dragée. Biscuit sugar plum powder toffee biscuit caramels. Wafer chupa chups danish pie cake carrot cake donut jujubes chocolate cake. Chupa chups icing bear claw sesame snaps soufflé icing. Ice cream jelly liquorice jujubes pastry dragée shortbread lollipop muffin."
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "#{portfolio_item} subtitle",
+    body: "Donut candy canes ice cream bear claw carrot cake sweet. Apple pie bear claw marzipan cotton candy bonbon donut macaroon cake. Soufflé candy canes cupcake chocolate gummies brownie dragée. Biscuit sugar plum powder toffee biscuit caramels. Wafer chupa chups danish pie cake carrot cake donut jujubes chocolate cake. Chupa chups icing bear claw sesame snaps soufflé icing. Ice cream jelly liquorice jujubes pastry dragée shortbread lollipop muffin.",
+    main_image: "https://via.placeholder.com/600",
+    thumb_image: "https://via.placeholder.com/350"
+  )
+end
+
+puts "9 portfolio items created"
